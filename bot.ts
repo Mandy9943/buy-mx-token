2dd54b69f0790437226102085e3a7fd756d2f8da
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-04-02 01:46:52 -0400

    buy blob updates

--- bot.ts
+++ bot.ts
@@ -1,8 +1,108 @@
 import BigNumber from "bignumber.js";
+import TelegramBot from "node-telegram-bot-api";
 import { e, envChain, World } from "xsuite";
 import data from "./data.json";
-
 require("dotenv").config();
+
+export const bot = new TelegramBot(process.env.TELEGRAM_BOT!, {
+  polling: true,
+});
+
+interface Pair {
+  address: string;
+  firstToken: {
+    balance: null;
+    decimals: number;
+    name: string;
+    identifier: string;
+    ticker: string;
+    owner: string;
+    assets: {
+      website: string;
+      description: string;
+      status: string;
+      pngUrl: string;
+      svgUrl: string;
+      __typename: string;
+    };
+    price: string;
+    type: string;
+    __typename: string;
+  };
+  firstTokenPrice: string;
+  firstTokenPriceUSD: string;
+  firstTokenVolume24h: string;
+  firstTokenLockedValueUSD: string;
+  secondToken: {
+    balance: null | string;
+    decimals: number;
+    name: string;
+    identifier: string;
+    ticker: string;
+    owner: string;
+    assets: {
+      website: string;
+      description: string;
+      status: string;
+      pngUrl: string;
+      svgUrl: string;
+      __typename: string;
+    };
+    price: string;
+    type: string;
+    __typename: string;
+  };
+  secondTokenPrice: string;
+  secondTokenPriceUSD: string;
+  secondTokenVolume24h: string;
+  secondTokenLockedValueUSD: string;
+  initialLiquidityAdder: string;
+  liquidityPoolToken: {
+    balance: null;
+    decimals: number;
+    name: string;
+    identifier: string;
+    ticker: string;
+    owner: string;
+    assets: {
+      website: null | string;
+      description: null | string;
+      status: null | string;
+      pngUrl: null | string;
+      svgUrl: null | string;
+      __typename: string;
+    };
+    price: string;
+    type: string;
+    __typename: string;
+  };
+  state: string;
+  type: string;
+  lockedValueUSD: string;
+  info: {
+    reserves0: string;
+    reserves1: string;
+    totalSupply: string;
+    __typename: string;
+  };
+  feesAPR: string;
+  feesUSD24h: string;
+  volumeUSD24h: string;
+  totalFeePercent: number;
+  specialFeePercent: number;
+  lockedTokensInfo: null;
+  feesCollector: null;
+  feeDestinations: any[];
+  trustedSwapPairs: any[];
+  whitelistedManagedAddresses: any[];
+  __typename: string;
+}
+
+const amountToBuy = 0.000001;
+const poolAddress =
+  "erd1qqqqqqqqqqqqqpgql8k7m0c5qegcp4lvknfawr8cchpgpksh2jps6cdnsm";
+
+const tokenToSend = "WEGLD-bd4d79";
 
 const world = World.new({
   chainId: envChain.id(),
@@ -13,6 +113,19 @@ const botWallet = () =>
     "bot-wallet.json",
     process.env.WALLET_PASSWORD!
   );
+
+const tryAgainOnError = async (func: () => Promise<any>) => {
+  try {
+    await func();
+  } catch (error: any) {
+    if (error.message === "fetch failed") {
+      console.error(error.message);
+      tryAgainOnError(func);
+    } else {
+      console.log("Error in tryAgainOnError", error.message);
+    }
+  }
+};
 
 // buy token
 const buyToken = async ({
@@ -33,7 +146,7 @@ const buyToken = async ({
   const result = await wallet.callContract({
     callee: scAddress,
     funcName: "swapTokensFixedInput",
-    gasLimit: 600_000_000,
+    gasLimit: 20_000_000,
     esdts: [
       {
         amount: amountToPay,
@@ -44,7 +157,7 @@ const buyToken = async ({
     funcArgs: [e.Str(tokenToBuy), e.U(minAmountToBuy)],
   });
 
-  console.log("Transaction:", result.tx.explorerUrl);
+  return result.tx;
 };
 
 const wrapEGLD = async (amount: number, shard: 0 | 1 | 2) => {
@@ -59,19 +172,6 @@ const wrapEGLD = async (amount: number, shard: 0 | 1 | 2) => {
   console.log("WrapEGLD Transaction:", result.tx.explorerUrl);
 };
 
-const tryAgainOnError = async (func: () => Promise<void>) => {
-  try {
-    await func();
-  } catch (error: any) {
-    if (error.message === "fetch failed") {
-      console.error(error.message);
-      tryAgainOnError(func);
-    } else {
-      console.log("Error in tryAgainOnError", error.message);
-    }
-  }
-};
-
 const buyTokenWithWEGLD = async (
   amount: number,
   tokenToBuy: string,
@@ -81,7 +181,7 @@ const buyTokenWithWEGLD = async (
   tryAgainOnError(() =>
     buyToken({
       amountToPay: new BigNumber(amount).times(1e18).toNumber(),
-      tokenToPay: "WEGLD-a28c59",
+      tokenToPay: tokenToSend,
       tokenToBuy: tokenToBuy,
       minAmountToBuy: new BigNumber(1)
         .times(10 ** tokenToBuyDecimals)
@@ -139,15 +239,83 @@ const fetchTransfersByTokenIdentifier = async (
   }
 };
 
-export const watchTransfersByTokenIdentifier = async (
-  tokenIdentifier: string
-) => {
-  setInterval(async () => {
-    const transfers = await fetchTransfersByTokenIdentifier(tokenIdentifier);
-    if (transfers.length > 3) {
-      buyTokenWithWEGLD(5, "token1123", 18, "");
-    }
-  }, 10000);
+const fetchPoolInfo = async (
+  addressPool: string
+): Promise<Pair | undefined> => {
+  const response = await fetch("https://graph.xexchange.com/graphql", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+      operationName: "activePoolsDataQuery",
+      variables: {
+        offset: 0,
+        limit: 500,
+      },
+      query:
+        "query activePoolsDataQuery($offset: Int!, $limit: Int!) {\n  pairs(offset: $offset, limit: $limit) {\n    address\n    firstToken {\n      balance\n      decimals\n      name\n      identifier\n      ticker\n      owner\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      price\n      type\n      __typename\n    }\n    firstTokenPrice\n    firstTokenPriceUSD\n    firstTokenVolume24h\n    firstTokenLockedValueUSD\n    secondToken {\n      balance\n      decimals\n      name\n      identifier\n      ticker\n      owner\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      price\n      type\n      __typename\n    }\n    secondTokenPrice\n    secondTokenPriceUSD\n    secondTokenVolume24h\n    secondTokenLockedValueUSD\n    initialLiquidityAdder\n    liquidityPoolToken {\n      balance\n      decimals\n      name\n      identifier\n      ticker\n      owner\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      price\n      type\n      __typename\n    }\n    state\n    type\n    lockedValueUSD\n    info {\n      reserves0\n      reserves1\n      totalSupply\n      __typename\n    }\n    feesAPR\n    feesUSD24h\n    volumeUSD24h\n    totalFeePercent\n    specialFeePercent\n    lockedTokensInfo {\n      lockingSC {\n        address\n        lockedToken {\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          decimals\n          name\n          collection\n          ticker\n          __typename\n        }\n        lpProxyToken {\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          decimals\n          name\n          collection\n          ticker\n          __typename\n        }\n        farmProxyToken {\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          decimals\n          name\n          collection\n          ticker\n          __typename\n        }\n        intermediatedPairs\n        intermediatedFarms\n        __typename\n      }\n      unlockEpoch\n      __typename\n    }\n    feesCollector {\n      address\n      __typename\n    }\n    feeDestinations {\n      address\n      tokenID\n      __typename\n    }\n    trustedSwapPairs\n    whitelistedManagedAddresses\n    __typename\n  }\n  lkmexProxies: proxy {\n    address\n    wrappedLpToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    wrappedFarmToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    lockedAssetTokens {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    assetToken {\n      balance\n      decimals\n      name\n      identifier\n      ticker\n      owner\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      price\n      type\n      __typename\n    }\n    intermediatedPairs\n    intermediatedFarms\n    __typename\n  }\n  simpleLockProxies: simpleLock {\n    address\n    lockedToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    lpProxyToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    farmProxyToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    intermediatedPairs\n    intermediatedFarms\n    __typename\n  }\n  simpleLockEnergy {\n    address\n    baseAssetToken {\n      balance\n      decimals\n      name\n      identifier\n      ticker\n      owner\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      price\n      type\n      __typename\n    }\n    lockedToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    legacyLockedToken {\n      assets {\n        website\n        description\n        status\n        pngUrl\n        svgUrl\n        __typename\n      }\n      decimals\n      name\n      collection\n      ticker\n      __typename\n    }\n    pauseState\n    lockOptions {\n      lockEpochs\n      penaltyStartPercentage\n      __typename\n    }\n    __typename\n  }\n  farms {\n    ... on FarmModelV2 {\n      address\n      farmToken {\n        assets {\n          website\n          description\n          status\n          pngUrl\n          svgUrl\n          __typename\n        }\n        decimals\n        name\n        collection\n        ticker\n        __typename\n      }\n      farmTokenPriceUSD\n      farmTokenSupply\n      farmingToken {\n        balance\n        decimals\n        name\n        identifier\n        ticker\n        owner\n        assets {\n          website\n          description\n          status\n          pngUrl\n          svgUrl\n          __typename\n        }\n        price\n        type\n        __typename\n      }\n      farmingTokenPriceUSD\n      farmedToken {\n        balance\n        decimals\n        name\n        identifier\n        ticker\n        owner\n        assets {\n          website\n          description\n          status\n          pngUrl\n          svgUrl\n          __typename\n        }\n        price\n        type\n        __typename\n      }\n      farmedTokenPriceUSD\n      pair {\n        address\n        firstToken {\n          balance\n          decimals\n          name\n          identifier\n          ticker\n          owner\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          price\n          type\n          __typename\n        }\n        firstTokenPriceUSD\n        secondToken {\n          balance\n          decimals\n          name\n          identifier\n          ticker\n          owner\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          price\n          type\n          __typename\n        }\n        secondTokenPriceUSD\n        liquidityPoolToken {\n          balance\n          decimals\n          name\n          identifier\n          ticker\n          owner\n          assets {\n            website\n            description\n            status\n            pngUrl\n            svgUrl\n            __typename\n          }\n          price\n          type\n          __typename\n        }\n        state\n        type\n        info {\n          reserves0\n          reserves1\n          totalSupply\n          __typename\n        }\n        feesAPR\n        totalFeePercent\n        specialFeePercent\n        __typename\n      }\n      state\n      version\n      penaltyPercent\n      perBlockRewards\n      totalValueLockedUSD\n      minimumFarmingEpochs\n      produceRewardsEnabled\n      baseApr\n      energyFactoryAddress\n      boostedYieldsRewardsPercenatage\n      boostedYieldsFactors {\n        maxRewardsFactor\n        userRewardsEnergy\n        userRewardsFarm\n        __typename\n      }\n      boosterRewards {\n        totalEnergyForWeek\n        __typename\n      }\n      optimalEnergyPerLp\n      __typename\n    }\n    __typename\n  }\n}\n",
+    }),
+  });
+
+  const data = await response.json();
+  if (data?.data?.pairs) {
+    const pairs = data.data.pairs as Pair[];
+    let blobPair = pairs.find((p) => p.address === addressPool);
+
+    return blobPair;
+  } else {
+    return undefined;
+  }
 };
+
+export const watchTransfersByTokenIdentifier = async (poolAddress: string) => {
+  const poolInfo = await fetchPoolInfo(poolAddress);
+  const isActive = poolInfo?.state === "Active";
+  if (poolInfo) {
+    console.log(
+      `Pool info founded <${poolInfo.firstToken.ticker} | ${poolInfo.secondToken.ticker}>`
+    );
+
+    if (isActive) {
+      console.log("Executing buy");
+      tryAgainOnError(async () => {
+        const tx = await buyToken({
+          amountToPay: new BigNumber(amountToBuy).times(1e18).toNumber(),
+          tokenToPay: poolInfo.secondToken.identifier,
+          tokenToBuy: poolInfo.firstToken.identifier,
+          minAmountToBuy: new BigNumber(1)
+            .times(10 ** poolInfo.firstToken.decimals)
+            .toNumber(),
+          scAddress: poolInfo.address,
+        });
+
+        bot.sendMessage(
+          709820730,
+          `
+         Compra efectuada
+         
+Url : ${tx.explorerUrl} 
+         `
+        );
+      });
+
+      return;
+    } else {
+      console.log("Skip buy");
+
+      setTimeout(() => watchTransfersByTokenIdentifier(poolAddress), 10000);
+    }
+  } else {
+    console.log("Pool not found, Skip buy");
+
+    setTimeout(() => watchTransfersByTokenIdentifier(poolAddress), 10000);
+  }
+};
+
+tryAgainOnError(() => watchTransfersByTokenIdentifier(poolAddress));
 
 // tryAgainOnError(() => wrapEGLD(3e18, 0));
